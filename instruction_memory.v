93810231d3d5ef8251bee17f086827479a574f21
ibtkm <ibtkm2009@gmail.com>
2015-04-01 12:12:32 +0430

    now we read instructions from file :D

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,11 +1,13 @@
 `timescale 1ns/1ns
-module InstructionMemory(input[11:0] address, input[18:0] data, input read_write, output reg[18:0] instruction);
+module InstructionMemory(input[11:0] address, output reg[18:0] instruction);
   reg[18:0] instructions[4095:0];
-  always @(read_write, address) begin
-    if(read_write)
-      instructions[address] = data;
-    else
-      instruction = instructions[address];
+
+  initial begin
+    $readmemb("instructions.mips", instructions);
+  end
+
+  always @(address) begin
+    instruction = instructions[address];
   end
 endmodule
 
