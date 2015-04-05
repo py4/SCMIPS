d49353ef6a18f94de23bbe2e2f051cf85384a60a
ibtkm <ibtkm2009@gmail.com>
2015-04-05 14:37:10 +0430

    carry out bug fix added

--- alu.v
+++ alu.v
@@ -7,34 +7,51 @@
 
 module ALU(input signed [7:0] A, input signed [7:0] B, input carry_in, is_shift, input [1:0] scode, input [2:0] acode, output reg [7:0] R, output reg zero, carry_out);
 
+  function get_carry_out; input [7:0] A, B, C;
+    get_carry_out = (~(C[7]))&(A[7]^B[7]) | A[7]&B[7]&C[7];
+  endfunction
+
+
+
   reg [8:0] temp;
   reg [15:0] temp2;
-
+  reg [7:0] neg_B;
   initial begin
     zero = 0;
     carry_out = 0;
   end
   always @(A, B, scode, acode) begin
+    neg_B = -B;
+    carry_out = 0;
     //$display("############# ALU ##############");
-    //  $display("A:  %b", A);
-
-//    $display("B:  %b", B);
-//    $display("scode:  %b", scode);
-//    $display("acode:  %b", acode);
     //$display("################################");
     case(is_shift)
       1'b0: begin
         case(acode)
-          3'b000: temp = A + B;
-          3'b001: temp = A + B + carry_in; 
-          3'b010: temp = A + ~B + 1;
-          3'b011: temp = A + ~B + 1 + carry_in;
+          3'b000: begin
+            temp = A + B;
+            carry_out = get_carry_out(A, B, temp);
+          end
+          3'b001: begin
+            temp = A + B + carry_in; 
+            carry_out = get_carry_out(A, B+carry_in, temp);
+          end
+          3'b010: begin
+            temp = A + ~B + 1;
+            carry_out = get_carry_out(A, -B, temp);
+          end
+          3'b011: begin
+            temp = A + ~B + 1 + carry_in;
+            carry_out = get_carry_out(A, -B + carry_in, temp);
+          end
+
           3'b100: temp = A & B;
           3'b101: temp = A | B;
           3'b110: temp = A ^ B;
         endcase
         R = temp[7:0];
-        carry_out = temp[8];
+        //carry_out = temp[8];
+        //carry_out = 0;
       end
       1'b1: 
         if(B == 8'b0)
@@ -63,6 +80,5 @@ module ALU(input signed [7:0] A, input signed [7:0] B, input carry_in, is_shift,
         end
     endcase
     zero = (R == 8'b0);
-  //  $display("*****  [alu]   R :   %b", R);
   end
 endmodule
